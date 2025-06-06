16fcd9b1ff60298bbbc5c03ca1d17b111e231277
luatne <luatdola365@gmail.com>
2025-06-06 12:52:00 +0700

    Update chatbotdemo.js

--- chatbotdemo.js
+++ chatbotdemo.js
@@ -303,7 +303,7 @@ style.innerHTML = `
     <div id="n8n-chat-messages"></div>
     <div id="n8n-suggest-buttons">
       <button class="n8n-suggest">🧠 Thông tin giới thiệu Công Ty Hoàng Bảo Khoa</button>
-      <button class="n8n-suggest">💰 top 5 sản phẩm đang giảm giá</button>
+      <button class="n8n-suggest">💰 thông tin sản phẩm đang giảm giá</button>
     </div>
     <div id="n8n-chat-input-container">
       <span id="n8n-input-menu">☰</span>
